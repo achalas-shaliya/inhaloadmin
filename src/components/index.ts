41deeb811efb935fc9f7fc20a83fe3a8c74f3580
achalas-shaliya <achalashaliya77@gmail.com>
2019-04-29 14:11:54 +0530

    Add All

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export * from './private-route/private-route';
+export * from './header/header';
+export * from './wrapper/wrapper';
+export * from './navigation-menu/navigation-menu';
+export * from './invoice-fields/invoice-fields';
+export * from './file-upload/file-upload';
+export * from './validation-list/validation-list';
