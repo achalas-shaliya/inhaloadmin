88a00ed8d16c094cd21ace5551609fa2065480d2
achalas-shaliya <achalashaliya77@gmail.com>
2019-04-29 14:42:22 +0530

    add all

---
+++ src/pages/blogs/index-page.js
@@ -0,0 +1,36 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+// import List from '@material-ui/core/List';
+import withStyles, { WithStyles } from '@material-ui/core/styles/withStyles';
+import Typography from '@material-ui/core/Typography';
+
+import * as React from 'react';
+import { HeaderComponent } from 'src/components';
+// import { mainListItems, secondaryListItems } from './listItems';
+// import SimpleLineChart from './SimpleLineChart';
+// import SimpleTable from './SimpleTable';
+import { styles } from './styles';
+
+class BlogIndex extends React.Component {
+  state = {
+    open: true,
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <HeaderComponent title='Blog' />
+        <main className={classes.content}>
+          <div className={classes.appBarSpacer} />
+          <Typography gutterBottom={true} component="h2">
+            Blog
+          </Typography>
+        </main>
+      </div>
+    );
+  }
+}
+
+export const BlogPage = withStyles(styles)(BlogIndex);
